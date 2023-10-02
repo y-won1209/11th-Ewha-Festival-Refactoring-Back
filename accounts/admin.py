542c8d44daf1a458143edb26f0f57d94b97963a5
yoonseoah <emma.yoon2002@gmail.com>
2023-10-03 02:03:35 +0900

    [feat] 회원가입, 로그인

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,5 +5,5 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-	list_display = ['id', 'username', 'nickname', 'password', 'is_booth', 'created_at', 'updated_at']
+	list_display = ['id', 'username', 'nickname', 'password', 'is_booth','is_show', 'is_tf', 'created_at', 'updated_at']
 	list_display_links = ['id', 'username', 'nickname']
--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     nickname=models.CharField(max_length=10)
     is_booth=models.BooleanField(default=False)
+    is_show=models.BooleanField(default=False)
     is_tf=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
