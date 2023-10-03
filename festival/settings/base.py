a71d1da22da43738f77cdc51bc6eaa9b37788935
ehyeji <jiyaaaa22@gamil.com>
2023-10-03 22:26:18 +0900

    Committing changes before merging

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
-
+from dotenv import load_dotenv
 
 import environ
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
