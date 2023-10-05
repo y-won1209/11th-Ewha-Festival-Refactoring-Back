fe12c5ab60d145d6d02b807378ef35334449aa14
ehyeji <jiyaaaa22@gamil.com>
2023-10-06 01:33:07 +0900

    fix: 코드 잘못 올라간거 수정

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import os
 from datetime import timedelta
-
+from dotenv import load_dotenv
 
 import environ
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
