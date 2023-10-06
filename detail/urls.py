c0720031e3767ba1452dc7538260afab068bb363
yewon <shimyewon@ewhain.net>
2023-10-06 12:09:53 +0900

    [fix]url

--- detail/urls.py
+++ detail/urls.py
@@ -5,6 +5,6 @@ app_name = "detail"
 
 urlpatterns = [
     path("<int:pk>/", EventDetailView.as_view()),
-    path("<int:event_id>/likes",EventLikeView.as_view()),
-    path("menu/<int:menu_id>/likes",MenuLikeViewv.as_view()),
+    path("<int:event_id>/likes/",EventLikeView.as_view()),
+    path("menu/<int:menu_id>/likes/",MenuLikeViewv.as_view()),
 ]
